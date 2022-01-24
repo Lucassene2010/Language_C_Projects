8862acba643f1159424d80146e94f679b0dd92c8
lucas.rgomes <lucas.gomes@alta-rt.com>
2022-01-24 18:39:24 -0300

    Exercise 1 from document attached

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 	Author: Lucas Henrique
 	Email: lucassene2010@gmail.com
 	GitHub: https://github.com/Lucassene2010
-	Exercise 37 from document attached
+	Exercise 1 from document attached
 */
 
 /////////////////////////////////////////////
@@ -10,10 +10,6 @@
 /////////////////////////////////////////////
 
 #include <stdio.h>
-#define OneToTwo ( (float) 1.00 )
-#define ThreeToFour ( (float) 1.40 )
-#define MoreFive ( (float) 2.00 )
-#define SixtyMinutes 60
 
 /////////////////////////////////////////////
 /// Global variables
@@ -39,46 +35,14 @@
 
 void main (void)
 {
-	float Multiplier,Time_pay;
-	int H_entry, M_entry, H_depart, M_depart;
-	int Time_entry, Time_depart, total_time;
-	
-	printf("\nInput the entry time\n");
-	printf("\nHours: ");
-	scanf("%d",&H_entry);
-	printf("\nMinutes: ");
-	scanf("%d",&M_entry);
-
-	printf("\nInput the departure time: \n");
-	printf("\nHours: ");
-	scanf("%d",&H_depart);
-	printf("\nMinutes: ");
-	scanf("%d",&M_depart);
-
-	Time_entry = M_entry + ( SixtyMinutes * H_entry );
-	Time_depart = M_depart + ( SixtyMinutes * H_depart );
-	total_time = Time_depart - Time_entry;
-
-	if( total_time >= SixtyMinutes * 5 )
+	float number1, number2;
+	printf("\ninput two numbers\nnumber1: ");
+	scanf("%f", &number1);
+	printf("\nnumber2: ");
+	scanf("%f", &number2);
+	if(number1 >= number2)
 	{
-		Multiplier = MoreFive;
+		printf("\nthe bigger number is: %.2f", number1);
 	}
-	else if( total_time >= SixtyMinutes * 3 )
-	{
-		Multiplier = ThreeToFour;
-	}
-	else
-	{
-		Multiplier = OneToTwo;
-	}
-
-	if( (total_time % SixtyMinutes) )
-	{
-		Time_pay = ( (float) (H_depart - H_entry) + 1.0 ) * Multiplier;
-	}
-	else
-	{
-		Time_pay = (float) (H_depart - H_entry) * Multiplier;
-	}
-	printf("The amount to be paied is: %.2f", Time_pay);
+	else printf("\nthe bigger number is: %.2f", number2);
 }
