f7f783701ebfbb2532df93ea26320cf493be2409
lucas.rgomes <lucas.gomes@alta-rt.com>
2022-01-31 17:07:29 -0300

    Exercise 31 from document attached

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 	Author: Lucas Henrique
 	Email: lucassene2010@gmail.com
 	GitHub: https://github.com/Lucassene2010
-	Exercise 2 from document attached
+	Exercise 31 from document attached
 */
 
 /////////////////////////////////////////////
@@ -18,8 +18,10 @@
 
 #include <stdio.h>
 #include <stdint.h>
-#define Number_To_Count 100
-#define First_Number_To_Count 1
+#define RangeToFirstCount (uint8_t) 1
+#define BootNumber (uint8_t) 1
+#define RangeToSecondCount (uint8_t) 2
+#define MaxCount (uint8_t) 50
 
 /////////////////////////////////////////////
 /// Global variables
@@ -45,21 +47,12 @@
 
 void main (void)
 {
-	uint8_t i;
 	system(CLEAR);
-	printf("\n\nCounting one to one hundred, one by one via comand repeat for: ");
-	for( i = (uint8_t) First_Number_To_Count ; i<=Number_To_Count ; i++ ) printf("%d, ",i);
-	printf("\n\nCounting one to one hundred, one by one via comand repeat while: ");
-	while(i >= First_Number_To_Count)
+	uint8_t i, j;
+	float sum;
+	for(i = BootNumber , j = BootNumber; i <= MaxCount; i += RangeToFirstCount, j += RangeToSecondCount)
 	{
-		i--;
-		printf("%d, ",Number_To_Count - i);
+		sum += ( (float) j / (float) i );
 	}
-	printf("\n\nCounting one to one hundred, one by one via comand repeat do-while: ");
-	do
-	{
-		i++;
-		printf("%d, ",i);
-
-	}while(i < Number_To_Count);
+	printf("sum: %.2f ", sum);
 }
